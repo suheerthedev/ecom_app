21cb787dc5ced64a2ae9bc22e3352cd856ffe5e7
Suheer Khan <khansuheer1@gmail.com>
2025-02-04 16:41:55 +0500

    Tried Changing Index of Notton NavBAr

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,17 +1,17 @@
-import 'package:ecom_app/app/app.bottomsheets.dart';
-import 'package:ecom_app/app/app.dialogs.dart';
 import 'package:ecom_app/app/app.locator.dart';
-import 'package:ecom_app/ui/common/app_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
-  final _dialogService = locator<DialogService>();
   final NavigationService navigationService = locator<NavigationService>();
-  final _bottomSheetService = locator<BottomSheetService>();
 
-  String get counterLabel => 'Counter is: $_counter';
+  int selectedIndex = 0;
+
+  void changeNavbarIndex(int index) {
+    selectedIndex = 1;
+    rebuildUi();
+  }
 
   List products = [
     {"name": "Regular Fit Slogan", "price": "1,190"},
@@ -23,7 +23,7 @@ class HomeViewModel extends BaseViewModel {
     {"name": "Regular Fit Slogan", "price": "1,190"},
   ];
 
-  List<Widget> tabs = [
+  List<Widget> tabs = const [
     Text("All"),
     Text("T-Shirts"),
     Text("Jeans"),
@@ -31,27 +31,4 @@ class HomeViewModel extends BaseViewModel {
     Text("Belts"),
     Text("Spectacles"),
   ];
-
-  int _counter = 0;
-
-  void incrementCounter() {
-    _counter++;
-    rebuildUi();
-  }
-
-  void showDialog() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.infoAlert,
-      title: 'Stacked Rocks!',
-      description: 'Give stacked $_counter stars on Github',
-    );
-  }
-
-  void showBottomSheet() {
-    _bottomSheetService.showCustomSheet(
-      variant: BottomSheetType.notice,
-      title: ksHomeBottomSheetTitle,
-      description: ksHomeBottomSheetDescription,
-    );
-  }
 }
