a77c67fc58f9aac4015a02c838d0f2f9b5e00732
Suheer Khan <khansuheer1@gmail.com>
2025-02-06 16:01:30 +0500

    Made A Product Model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String title;
+  final String imagePath;
+  final double price;
+  final double rating;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.imagePath,
+    required this.price,
+    required this.rating,
+  });
+}
